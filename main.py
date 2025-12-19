c539ba28ee817316eff8e6b83dfa8b773c3b9f57
Eswar Krishna Maganti <maganti.ek@gmail.com>
2025-12-19 08:04:54 +0530

    Created a single route to fetch and display products from DB

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import os
+
+# load the environment variables
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DB = os.getenv('MYSQL_DB')
+
 
 app = Flask(__name__)
 
+app.config['MYSQL_USER'] = MYSQL_USER
+app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
+app.config['MYSQL_DB'] = MYSQL_DB
+app.config['MYSQL_HOST'] = MYSQL_HOST
+
+mysql = MySQL(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' SELECT * FROM products ''')
+    products =  cursor.fetchall()
+    return render_template('index.html', products=products)
